e4967e61f33e5ab4e068eeb1e08d4e641760c381
Aimee Taylor <AimeeJATDeveloper@outlook.com>
2024-02-29 13:29:44 +0000

    Created fetch url function to call weather api

--- background/background.js
+++ background/background.js
@@ -1,1 +1,16 @@
 const weatherApiUrl = "https://api.open-meteo.com/v1/forecast?latitude=49.4585&longitude=-2.5787&hourly=temperature_2m";
+
+async function fetchUrl () {
+    try {
+        const response = await fetch(weatherApiUrl);
+
+        if (!response.ok) {
+            throw new Error("Issue with network response")
+        }
+        return response.json;
+
+    }
+    catch(error) {
+        console.log("Error:", error);
+    }
+}
