67990b460603c5210ebedabf0808e874c7d85e76
Aimee Taylor <AimeeJATDeveloper@outlook.com>
2024-03-06 12:50:42 +0000

    Completed datetime function in background.js

--- background/background.js
+++ background/background.js
@@ -1,26 +1,38 @@
 const weatherApiUrl = "https://api.open-meteo.com/v1/forecast?latitude=49.4585&longitude=-2.5787&hourly=temperature_2m";
 
-async function fetchUrl () {
+async function fetchUrl() {
     try {
         const response = await fetch(weatherApiUrl);
 
         if (!response.ok) {
             throw new Error("Issue with network response")
         }
+        
         return response.json;
 
     }
+
     catch(error) {
-        console.log("Error:", error);
+        console.log(error);
     }
 }
 
-const date = new Date();
-
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
 
 
-let currentDate = `${day}-${month}-${year}`;
-console.log(currentDate);
+
+function datetime() {
+    const date = new Date();
+
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    let currentDate = `${day}-${month}-${year}`;
+    console.log(currentDate);
+
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+
+    let currentTime = `${hours}:${minutes}`;
+    console.log(currentTime);
+}
