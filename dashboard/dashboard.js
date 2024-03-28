71df44c5a600b18dca017ceb6ec3d4045471fe0c
Aimee Taylor <AimeeJATDeveloper@outlook.com>
2024-03-28 14:02:32 +0000

    Got the weather map api to work using a different method

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,9 +1,10 @@
 import * as maptilersdk from '@maptiler/sdk';
 
-maptilersdk.config.apiKey = 'gm2EYqR1nDRlUcaiw7nu ';
+maptilersdk.config.apiKey = 'gm2EYqR1nDRlUcaiw7nu';
 const map = new maptilersdk.Map({
   container: 'map-div', // container's id or the HTML element to render the map
-  style: maptilersdk.MapStyle.STREETS,
+  style: "backdrop",
   center: [16.62662018, 49.2125578], // starting position [lng, lat]
   zoom: 14, // starting zoom
 });
+
