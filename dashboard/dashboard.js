1b15b77d9564d8f981701e6c4cabdc67e8bad618
Aimee Taylor <AimeeJATDeveloper@outlook.com>
2024-04-04 13:03:41 +0100

    Working out how to get the data from api using JSON

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,2 +1,21 @@
+const weatherApiUrl = "https://api.open-meteo.com/v1/forecast?latitude=49.4585&longitude=-2.5787&hourly=temperature_2m";
 
+async function fetchUrl() {
+    try {
+        const response = await fetch(weatherApiUrl);
 
+        if (!response.ok) {
+            throw new Error("Issue with network response")
+        }
+        
+        const data =  await response.json();
+        console.log(data.timezone);
+
+    }
+
+    catch(error) {
+        console.log(error);
+    }
+}
+
+fetchUrl()
