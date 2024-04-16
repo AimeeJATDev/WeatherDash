d8fdb30cff4c20d4962b8cda227a87b1b89e15e4
Aimee Taylor <AimeeJATDeveloper@outlook.com>
2024-04-16 13:09:27 +0100

    Used geocoding api to get latitude and longitude values, just now need to hook it up to getWeatherData() function

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,10 +1,10 @@
-const weatherApiUrl = "https://api.open-meteo.com/v1/forecast?";
 
-async function getWeatherData() {
+async function getWeatherData(values) {
+    const weatherApiUrl = "https://api.open-meteo.com/v1/forecast?";
     try {
         const response = await fetch (weatherApiUrl + new URLSearchParams({
-            "latitude": 52.52,
-            "longitude": 13.48,
+            "latitude": values[0],
+            "longitude": values[1],
             "current": ["temperature_2m", "weather_code"]
         }))
 
@@ -22,10 +22,46 @@ async function getWeatherData() {
     }
 }
 
+async function findLocation(location) {
+    const geocodingApiUrl = "https://us1.locationiq.com/v1/search?";
+    const apiKey = "pk.94745bb3fc90ed960d50ca389a48961c"
+    try {
+        const response = await fetch(geocodingApiUrl + new URLSearchParams({
+            "key": apiKey,
+            "country": location,
+            "format": "json"
+        }));
+
+        /*if (!response.ok) {
+            throw new Error("Issue with network response");
+        }*/
+
+        const data = await response.json();
+
+        console.log(data[0].lat);
+        console.log(data[0].lon);
+
+
+        let latitude = data[0].lat;
+        let longitude = data[0].lon;
+
+        values = [latitude, longitude]
+
+        return values;
+
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
+
 let locationForm = document.getElementById("location-form");
 
 locationForm.addEventListener("submit", (e) => {
     e.preventDefault();
+    let inputField = document.getElementById("location-field").value;
+    
+    findLocation(inputField)
     getWeatherData();
 
 });
