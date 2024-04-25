b54ae0ad2001669e3a01b9f03599c6be7099a843
Aimee Taylor <AimeeJATDeveloper@outlook.com>
2024-04-25 12:59:06 +0100

    Completed weather code object

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -126,6 +126,7 @@ async function autocomplete(input) {
 }
 
 function decodeWeather(weatherCode) {
+    /* Weather Codes and Descriptions from: https://open-meteo.com/en/docs */
     weatherDict = {
         "0": "Clear Sky",
         "1": "Mainly Clear",
@@ -133,7 +134,28 @@ function decodeWeather(weatherCode) {
         "3": "Overcast",
         "45": "Fog",
         "48": "Depositing Rime Fog",
-        "51": "Light Drizzle"
+        "51": "Light Drizzle",
+        "53": "Moderate Drizzle",
+        "55": "Dense Drizzle",
+        "56": "Light Freezing Drizzle",
+        "57": "Dense Freezing Drizzle",
+        "61": "Slight Rain",
+        "63": "Moderate Rain",
+        "65": "Heavy Rain",
+        "66": "Light Freezing Rain",
+        "67": "Heavy Freezing Rain",
+        "71": "Slight Snowfall",
+        "73": "Moderate Snowfall",
+        "75": "Heavy Snowfall",
+        "77": "Snow Grains",
+        "80": "Slight Rain Showers",
+        "81": "Moderate Rain Showers",
+        "82": "Violent Rain Showers",
+        "85": "Slight Snow Showers",
+        "86": "Heavy Snow Showers",
+        "95": "Thunderstorm",
+        "96": "Thunderstorm with Slight Hail",
+        "99": "Thunderstorm with Heavy Hail"
     }
 
     for (const [key, value] of Object.entries(weatherDict)) {
